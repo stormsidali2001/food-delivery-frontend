8a4a8a8af5e237cbbba559bda70d4675f6d23400
stormsidali2001 <assoulsidali@gmail.com>
2022-07-13 19:49:33 +0100

    ....

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,5 +1,13 @@
 import styles from '../styles/Sidebar.module.scss'
 import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonIcon from '@mui/icons-material/Person';
+import Inventory2Icon from '@mui/icons-material/Inventory2';
+import AnalyticsIcon from '@mui/icons-material/Analytics';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import GroupIcon from '@mui/icons-material/Group';
+import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
+import DeliveryDiningIcon from '@mui/icons-material/DeliveryDining';
+import MonitorHeartIcon from '@mui/icons-material/MonitorHeart';
 const links = [
   {
     text:'Dashboard',
@@ -7,10 +15,52 @@ const links = [
   },
   {
     text:'Users',
-    icon: <DashboardIcon/>
+    icon: <GroupIcon/>
+  },
+  {
+    text:'Products',
+    icon: <Inventory2Icon/>
+  },
+  {
+    text:'Statistics',
+    icon: <AnalyticsIcon/>
+  },
+  {
+    text:'Delivery',
+    icon: <DeliveryDiningIcon/>
+  },
+  {
+    text:'Notifications',
+    icon: <NotificationsIcon/>
+  },
+  {
+    text:'System health',
+    icon: <MonitorHeartIcon/>
+  },
+  {
+    text:'Profil',
+    icon: <PersonIcon/>
+  },
+  {
+    text:'Logout',
+    icon: <MeetingRoomIcon/>
   },
 
 ]
+const getSection = (index:number)=>{
+    switch(index){
+      case 0:
+        return 'MAIN'
+      case 1:
+        return 'LISTS'
+      case 5:
+        return 'USEFUL'
+      case 7:
+        return 'USER'
+      default:
+        return null
+    }
+}
 export const SideBar = () => {
   return (
     <div className={styles.container}>
@@ -21,12 +71,20 @@ export const SideBar = () => {
        <div className={styles.center}>
             <ul>
                {
-                links.map(({text,icon})=>{
+                links.map(({text,icon},index)=>{
+                  const section = getSection(index);
                   return(
-                    <li>
-                      {icon}
-                      <span>{text}</span>
-                    </li>
+                    <>
+                      {
+                        section&&(
+                          <p className={styles.title}>{section}</p>
+                        )
+                      }
+                      <li>
+                        {icon}
+                        <span>{text}</span>
+                      </li>
+                    </>
                   )
                 })
                }

cdbdc0a17e3a845cb1e619713f3285e887ce77c4
stormsidali2001 <assoulsidali@gmail.com>
2022-07-14 15:04:37 +0100

    widget component ...

--- components/Widget.tsx
+++ components/Widget.tsx
@@ -1,6 +1,8 @@
 import { KeyboardArrowUp } from '@mui/icons-material'
 import styles from '../styles/Widget.module.scss'
+import PersonIcon from '@mui/icons-material/Person';
 const Widget = () => {
+    
   return (
     <div className={styles.container}>
         <div className={styles.left}>
@@ -9,10 +11,11 @@ const Widget = () => {
             <span className={styles.link}>see all user</span>
         </div>
         <div className={styles.right}>
-            <div className="percentage">
+            <div className={`${styles.percentage} ${styles.positive}`}>
                 <KeyboardArrowUp/>
                 20%
             </div>
+            <PersonIcon className={styles.icon}/>
         </div>
     </div>
   )

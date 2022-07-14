4c91a81d282ca39bbcfc2ed4089e28a5ecbc3b37
stormsidali2001 <assoulsidali@gmail.com>
2022-07-14 14:35:59 +0100

    widget component

--- components/Widget.tsx
+++ components/Widget.tsx
@@ -1,8 +1,19 @@
+import { KeyboardArrowUp } from '@mui/icons-material'
 import styles from '../styles/Widget.module.scss'
 const Widget = () => {
   return (
     <div className={styles.container}>
-
+        <div className={styles.left}>
+            <span className={styles.title}>USERS</span>
+            <span className={styles.counter}>12554</span>
+            <span className={styles.link}>see all user</span>
+        </div>
+        <div className={styles.right}>
+            <div className="percentage">
+                <KeyboardArrowUp/>
+                20%
+            </div>
+        </div>
     </div>
   )
 }
